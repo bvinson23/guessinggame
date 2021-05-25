d03ad21a5fd311c115a5f2d0148c74220d65c4ec
bvinson23 <brandonvinson23@gmail.com>
2021-05-25 13:36:44 -0500

    all three dificulties work

--- Program.cs
+++ Program.cs
@@ -59,6 +59,7 @@ void Play()
         }
     }
     else if (playDifficulty == 2)
+    {
         while (numberOfGuesses < 6)
         {
             Console.WriteLine("Guess the secret number");
@@ -97,4 +98,46 @@ void Play()
             }
             Game();
         }
+    }
+    else if (playDifficulty == 3)
+    {
+        while (numberOfGuesses < 4)
+        {
+            Console.WriteLine("Guess the secret number");
+            Console.WriteLine("-----------------------");
+            Console.Write($"Your guess({numberOfGuesses + 1})");
+            string userGuess = Console.ReadLine();
+
+            void Game()
+            {
+                if (secretNumber == int.Parse(userGuess))
+                {
+                    Console.WriteLine("Whoa, can you read my mind?");
+                    numberOfGuesses = 5;
+                }
+                else
+                {
+                    if (int.Parse(userGuess) > secretNumber)
+                    {
+                        Console.WriteLine("Too high. Try again.");
+                        numberOfGuesses++;
+                        Console.WriteLine($"You have {4 - numberOfGuesses} left.");
+                        Console.WriteLine();
+                    }
+                    else if (int.Parse(userGuess) < secretNumber)
+                    {
+                        Console.WriteLine("Too low. Try again.");
+                        numberOfGuesses++;
+                        Console.WriteLine($"You have {4 - numberOfGuesses} left.");
+                        Console.WriteLine();
+                    }
+                }
+                if (numberOfGuesses == 4)
+                {
+                    Console.WriteLine("No more guesses, you lose!");
+                }
+            }
+            Game();
+        }
+    }
 }
