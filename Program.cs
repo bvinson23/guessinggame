a3df2a0577441842bcd6749e1618d358c1b03c70
bvinson23 <brandonvinson23@gmail.com>
2021-05-25 13:33:54 -0500

    easy and medium difficulties are set up

--- Program.cs
+++ Program.cs
@@ -6,45 +6,95 @@ void Play()
     int numberOfGuesses = 0;
     Random i = new Random();
     int secretNumber = i.Next(1, 100);
-    while (numberOfGuesses < 4)
-    {
-        Console.WriteLine("-----------------");
-        Console.WriteLine("| Guessing Game |");
-        Console.WriteLine("-----------------");
-        Console.WriteLine("Guess the secret number");
-        Console.WriteLine("-----------------------");
-        Console.Write($"Your guess({numberOfGuesses + 1})");
-        string userGuess = Console.ReadLine();
+    Console.WriteLine("-----------------");
+    Console.WriteLine("| Guessing Game |");
+    Console.WriteLine("-----------------");
+    Console.WriteLine("Select your difficulty.");
+    Console.WriteLine("1) Easy");
+    Console.WriteLine("2) Medium");
+    Console.WriteLine("3) Hard");
 
-        void Game()
+    string difficulty = Console.ReadLine();
+    int playDifficulty = int.Parse(difficulty);
+
+    if (playDifficulty == 1)
+    {
+        while (numberOfGuesses < 8)
         {
-            if (secretNumber == int.Parse(userGuess))
+            Console.WriteLine("Guess the secret number");
+            Console.WriteLine("-----------------------");
+            Console.Write($"Your guess({numberOfGuesses + 1})");
+            string userGuess = Console.ReadLine();
+
+            void Game()
             {
-                Console.WriteLine("Whoa, can you read my mind?");
-                numberOfGuesses = 5;
-            }
-            else
-            {
-                if (int.Parse(userGuess) > secretNumber)
+                if (secretNumber == int.Parse(userGuess))
                 {
-                    Console.WriteLine("Too high. Try again.");
-                    numberOfGuesses++;
-                    Console.WriteLine($"You have {4 - numberOfGuesses} left.");
-                    Console.WriteLine();
+                    Console.WriteLine("Whoa, can you read my mind?");
+                    numberOfGuesses = 9;
                 }
-                else if (int.Parse(userGuess) < secretNumber)
+                else
                 {
-                    Console.WriteLine("Too low. Try again.");
-                    numberOfGuesses++;
-                    Console.WriteLine($"You have {4 - numberOfGuesses} left.");
-                    Console.WriteLine();
+                    if (int.Parse(userGuess) > secretNumber)
+                    {
+                        Console.WriteLine("Too high. Try again.");
+                        numberOfGuesses++;
+                        Console.WriteLine($"You have {8 - numberOfGuesses} left.");
+                        Console.WriteLine();
+                    }
+                    else if (int.Parse(userGuess) < secretNumber)
+                    {
+                        Console.WriteLine("Too low. Try again.");
+                        numberOfGuesses++;
+                        Console.WriteLine($"You have {8 - numberOfGuesses} left.");
+                        Console.WriteLine();
+                    }
+                }
+                if (numberOfGuesses == 8)
+                {
+                    Console.WriteLine("No more guesses, you lose!");
                 }
             }
-            if (numberOfGuesses == 4)
-            {
-                Console.WriteLine("No more guesses, you lose!");
-            }
+            Game();
         }
-        Game();
     }
+    else if (playDifficulty == 2)
+        while (numberOfGuesses < 6)
+        {
+            Console.WriteLine("Guess the secret number");
+            Console.WriteLine("-----------------------");
+            Console.Write($"Your guess({numberOfGuesses + 1})");
+            string userGuess = Console.ReadLine();
+
+            void Game()
+            {
+                if (secretNumber == int.Parse(userGuess))
+                {
+                    Console.WriteLine("Whoa, can you read my mind?");
+                    numberOfGuesses = 7;
+                }
+                else
+                {
+                    if (int.Parse(userGuess) > secretNumber)
+                    {
+                        Console.WriteLine("Too high. Try again.");
+                        numberOfGuesses++;
+                        Console.WriteLine($"You have {6 - numberOfGuesses} left.");
+                        Console.WriteLine();
+                    }
+                    else if (int.Parse(userGuess) < secretNumber)
+                    {
+                        Console.WriteLine("Too low. Try again.");
+                        numberOfGuesses++;
+                        Console.WriteLine($"You have {6 - numberOfGuesses} left.");
+                        Console.WriteLine();
+                    }
+                }
+                if (numberOfGuesses == 6)
+                {
+                    Console.WriteLine("No more guesses, you lose!");
+                }
+            }
+            Game();
+        }
 }
