876dddd0f49b4551cb394dd0f48ad77bed4451f4
bvinson23 <brandonvinson23@gmail.com>
2021-05-25 11:37:46 -0500

    phase 3 complete

--- Program.cs
+++ Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 
-Console.Write("Guess the secret number >>");
-string userGuess = Console.ReadLine();
-
-int secretNumber = 42;
-
-if (secretNumber == int.Parse(userGuess))
+Play();
+void Play()
 {
-    Console.WriteLine("Whoa, can you read my mind?");
-}
-else
-{
-    Console.WriteLine("Seriously, that's your guess? Nope.");
+    int numberOfGuesses = 0;
+    int secretNumber = 42;
+    while (numberOfGuesses < 4)
+    {
+        Console.WriteLine("Guess the secret number");
+        Console.WriteLine("-----------------------");
+        string userGuess = Console.ReadLine();
+
+        void Game()
+        {
+            if (secretNumber == int.Parse(userGuess))
+            {
+                Console.WriteLine("Whoa, can you read my mind?");
+                numberOfGuesses = 5;
+            }
+            else
+            {
+                Console.WriteLine("Seriously, that's your guess? Try again.");
+                numberOfGuesses++;
+            }
+            if (numberOfGuesses == 4)
+            {
+                Console.WriteLine("No more guesses, you lose!");
+            }
+        }
+        Game();
+    }
 }
